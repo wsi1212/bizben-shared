4d59e1c829d79c7b89dd016310dd04438485d236
wsi1212 <wsi1212@dgsw.hs.kr>
2024-08-12 12:38:52 -0700

    add: Add business, user model

---
+++ dist/User.d.ts
@@ -0,0 +1,20 @@
+export interface User {
+    userId: string;
+    aboutCompany?: string;
+    aboutUser?: string;
+    accountID?: string;
+    address?: string;
+    city?: string;
+    companyName?: string;
+    counties?: string[];
+    createdAt: string;
+    email?: string;
+    firstName?: string;
+    lastName?: string;
+    legacyActive?: boolean;
+    phoneNo?: string;
+    profile?: string;
+    profileImageUrl?: string;
+    type?: string;
+    website?: string;
+}
